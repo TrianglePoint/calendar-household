306a736d5e8e135e294fb50b82818b402c3be897
TrianglePoint <rlarbgus10@gmail.com>
2019-09-18 20:55:59 +0900

    Http통신(진행중)
    WebApiConfig에서 주소 설정중

---
+++ CH/CH.Client/Common/HttpCommunicator.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace CH.Client.Common
+{
+    public class HttpCommunicator
+    {
+        private readonly HttpWebRequest request;
+
+        public HttpCommunicator()
+        {
+            string uriString = "";
+
+            request = (HttpWebRequest)WebRequest.Create(uriString);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Get(string, )
+        {
+            request = 
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+
+        }
+    }
+}
