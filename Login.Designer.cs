fb290d898ff230c9b3f9238996a7f1f09b36fb4e
KangMoonsuk <30399969+KangMoonsuk@users.noreply.github.com>
2019-11-21 22:22:04 +0900

    로그인 화면 설정
    로그인 화면입니다.
    유저 아이디 비밀번호 임의로 admin/pass로 써놨습니다.

---
+++ Login.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CH.Client.Views
+{
+    partial class LogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.lblAccount = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(14, 244);
+            this.btnLogIn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(366, 40);
+            this.btnLogIn.TabIndex = 6;
+            this.btnLogIn.Text = "ログイン";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.BtnLogIn_Click);
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.AutoSize = true;
+            this.lblAccount.Location = new System.Drawing.Point(11, 89);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(92, 17);
+            this.lblAccount.TabIndex = 2;
+            this.lblAccount.Text = "アカウント名";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(11, 159);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(75, 17);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "パスワード";
+            // 
+            // txtAccount
+            // 
+            this.txtAccount.Location = new System.Drawing.Point(14, 109);
+            this.txtAccount.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAccount.Name = "txtAccount";
+            this.txtAccount.Size = new System.Drawing.Size(365, 22);
+            this.txtAccount.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(14, 179);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPassword.MaxLength = 10;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(365, 22);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTitle.Location = new System.Drawing.Point(94, 29);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(231, 27);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "カレンダー家計簿";
+            // 
+            // LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 309);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtAccount);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblAccount);
+            this.Controls.Add(this.btnLogIn);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "LogIn";
+            this.Text = "LogIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtAccount;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CH.Client.Views
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogIn_Click(object sender, EventArgs e)
+        {
+            if(!checkError()){
+                //임시 로그인 아이디 : admin 
+                if (txtAccount.Text.Equals("admin") && txtPassword.Text.Equals("pass"))
+                {
+                    this.Visible = false;
+                    Main main = new Main();
+                    main.ShowDialog();
+                    this.Close();
+                }
+
+            }
+        }
+
+        private bool checkError()
+        {
+            if (string.IsNullOrEmpty(txtAccount.Text) || string.IsNullOrEmpty(txtPassword.Text))
+            {
+                MessageBox.Show("IDまたはPASSWORDOOを入力してください。", "",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                txtAccount.Focus();
+                return true;
+            }
+            if (!txtAccount.Text.Equals("admin")|| !txtPassword.Text.Equals("pass"))
+            {
+                MessageBox.Show("一致する会員情報がありません。", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPassword.Focus();
+                return true;
+            }
+            return false;
+        }
+    }
+}
